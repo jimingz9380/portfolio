5f459394484635fd8163b9b01909696fe884d54c
Jiming Zheng <jimingz9380@gmail.com>
2022-08-05 17:01:37 -0400

    changeable webpage

--- data.js
+++ data.js
@@ -1,25 +1,27 @@
 let projectdata = [
     {
         "projectname":{
-            "name1": "choose your adventure",
-            "name2": "freedom Project",
-            "name3": "Hello World"
+            "name1": "Choose your adventure",
+            "name2": "Freedom Project",
+            "name3": "Fake Movie"
         },
 
         "projectimg":{
-            "img1": "#",
-            "img2": "#",
-            "img3": "#"
+            "img1": "https://jimingz9380.github.io/sep10-becoming-a-god-cyoa-/",
+            "img2": "https://jimingz9380.github.io/projects/sep10-fp.html",
+            "img3": "https://epic-movie--safen2614.repl.co/"
         },
 
         "projectcontext":{
-
+            "cyoa": "This is when I was in 10th grade using learing ide and using the ide to make a adventure sort of game. It was a long adventure so it a long working progress game.",
+            "fp": "My entire School year was about me making a final website to see what I learned from the entre school year, this came with many challenge. It is mostly about how technology have affect Business.",
+            "fake_movie": "This project is about a creative movie idea and use what we learn to put inside the project"
         },
 
         "projectlink":{
-            "link1": "#",
-            "link2": "#",
-            "link3": "#"
+            "link1": "https://jimingz9380.github.io/projects/cyoa.html",
+            "link2": "https://jimingz9380.github.io/projects/sep10-fp.html",
+            "link3": "https://epic-movie--safen2614.repl.co/"
         }
 
     }
--- main.js
+++ main.js
@@ -1,11 +1,52 @@
 let project1 = document.querySelector("#submit-btn1")
 project1.onclick = switchProject1;
 
+let project2 = document.querySelector("#submit-btn2")
+project2.onclick = switchProject2;
+
+let project3 = document.querySelector("#submit-btn3")
+project3.onclick = switchProject3;
+
 function switchProject1(){
 
     let P = projectdata[0];
 
     let projecthead1 = document.getElementById("h1head");
-    projecthead1.innerHTML = `Project:${P.projectname.name1} is the best`;
+    projecthead1.innerHTML = `Project:${P.projectname.name1} `;
+
+    let pImg = document.querySelector("#img");
+    pImg.src = `${P.projectimg.img1}.`;
+
+    let description = document.getElementById("context");
+    description.innerHTML = `${P.projectcontext.cyoa}`;
+}
+
+function switchProject2(){
+
+    let P = projectdata[0];
+
+    let projecthead1 = document.getElementById("h1head");
+    projecthead1.innerHTML = `Project:${P.projectname.name2} `;
+
+    let pImg = document.querySelector("#img");
+    pImg.src = `${P.projectimg.img2}`;
+
+    let description = document.getElementById("context");
+    description.innerHTML = `${P.projectcontext.fp}`;
+
+}
+
+function switchProject3(){
+
+    let P = projectdata[0];
+
+    let projecthead1 = document.getElementById("h1head");
+    projecthead1.innerHTML = `Project:${P.projectname.name3} `;
+
+    let pImg = document.querySelector("#img");
+    pImg.src = `${P.projectimg.img3}.`;
+
+    let description = document.getElementById("context");
+    description.innerHTML = `${P.projectcontext.fake_movie}`;
 
 }
